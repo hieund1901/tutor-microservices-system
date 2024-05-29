d5540562134952d308aa6916e7e3f05468365207
sonld <son.ld24699@gmail.com>
2024-05-29 12:42:38 +0700

    minio

--- common-utils/src/main/java/com/microservices/projectfinal/util/MediaFileUtils.java
+++ common-utils/src/main/java/com/microservices/projectfinal/util/MediaFileUtils.java
@@ -4,6 +4,7 @@ import com.microservices.projectfinal.model.VideoInformationModel;
 import io.minio.BucketExistsArgs;
 import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import net.bramp.ffmpeg.FFprobe;
@@ -33,7 +34,23 @@ public class MediaFileUtils {
 //        Path fileNameAndPath = Paths.get(IMAGE_DIR, imageName);
 //        Files.write(fileNameAndPath, file.getBytes());
 //        return fileNameAndPath.toString();
+        MinioClient minioClient  = MinioClient.builder()
+                .endpoint("http://localhost:9000")
+                .credentials("QWBfiGKTIxn8ADIIJoKj", "lAQkhF4vK8hH8avxiWlIu3JIhW4c5JGx1rKThDqZ")
+                .build();
+        if (!minioClient.bucketExists(BucketExistsArgs.builder().bucket(IMAGE_BUCKET).build())) {
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket(IMAGE_BUCKET).build());
+        }
+        String fileName = UUID.randomUUID() + file.getOriginalFilename();
+        minioClient.putObject(
+                PutObjectArgs.builder()
+                        .bucket(IMAGE_BUCKET)
+                        .object(fileName)
+                        .stream(file.getInputStream(), file.getSize(), -1)
+                        .build()
+        );
 
+        return fileName;
     }
 
     @SneakyThrows
@@ -41,16 +58,32 @@ public class MediaFileUtils {
         if (video == null || video.isEmpty()) return null;
         String extension = video.getOriginalFilename().split("\\.")[1];
         String videoName = UUID.randomUUID() + "." + extension;
-        Path path = Paths.get(VIDEO_DIR);
+        Path path = Paths.get(VIDEO_BUCKET);
         if (!Files.exists(path)) Files.createDirectories(path);
-        Path fileNameAndPath = Paths.get(VIDEO_DIR, videoName);
+        Path fileNameAndPath = Paths.get(VIDEO_BUCKET, videoName);
         Files.write(fileNameAndPath, video.getBytes());
         long duration = getVideoDuration(fileNameAndPath.toString());
+        Files.delete(fileNameAndPath);
+        MinioClient minioClient  = MinioClient.builder()
+                .endpoint("http://localhost:9000")
+                .credentials("QWBfiGKTIxn8ADIIJoKj", "lAQkhF4vK8hH8avxiWlIu3JIhW4c5JGx1rKThDqZ")
+                .build();
+        if (!minioClient.bucketExists(BucketExistsArgs.builder().bucket(VIDEO_BUCKET).build())) {
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket(VIDEO_BUCKET).build());
+        }
+        minioClient.putObject(
+                PutObjectArgs.builder()
+                        .bucket(VIDEO_BUCKET)
+                        .object(videoName)
+                        .stream(video.getInputStream(), video.getSize(), -1)
+                        .build()
+        );
         return VideoInformationModel.builder()
                 .duration(duration)
                 .title(videoName)
-                .path(fileNameAndPath.toString())
+                .path(videoName)
                 .build();
+
     }
 
     @SneakyThrows
@@ -63,9 +96,9 @@ public class MediaFileUtils {
         return (long) probeResult.getFormat().duration;
     }
 
-    @SneakyThrows
-    private void makeBucket(String bucketName) {
-        boolean found = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
-        if (!found) minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
-    }
+//    @SneakyThrows
+//    private void makeBucket(String bucketName) {
+//        boolean found = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+//        if (!found) minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+//    }
 }
